c263a0b8aab55afb5bea26cb14ed06d36c267b78
KainTenebris <43878231+KainTenebris@users.noreply.github.com>
2018-11-16 12:04:46 +0000

    Create ObjectiveItem
    fetches implemented
    use not implemented

---
+++ core/src/com/uncreative/game/ObjectiveItem.java
@@ -0,0 +1,32 @@
+package com.uncreative.game;
+
+public class ObjectiveItem {
+	private Objective objective;
+	private Integer goldWorth;
+	private Integer xpRequired;
+	private Integer uses;
+	
+	public ObjectiveItem(Objective objective, Integer gold, Integer xp, Integer uses) {
+		this.objective = objective;
+		this.goldWorth = gold;
+		this.xpRequired = xp;
+		this.uses = uses;
+	}
+	
+	public Objective getObjective() {
+		return this.objective;
+	}
+	public Integer getGold() {
+		return this.goldWorth;
+	}
+	public Integer getXP() {
+		return this.xpRequired;
+	}
+	public Integer usesLeft() {
+		return this.uses;
+	}
+	
+	public Boolean use() {
+		return false;
+	}
+}
