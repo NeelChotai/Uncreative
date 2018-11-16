7b343bd0ee9ba2b8a0584060fbec7ad77b4afca6
CameronB99 <43375791+CameronB99@users.noreply.github.com>
2018-11-16 12:15:04 +0000

    Update DesktopLauncher.java

--- desktop/src/com/uncreative/game/desktop/DesktopLauncher.java
+++ desktop/src/com/uncreative/game/desktop/DesktopLauncher.java
@@ -7,6 +7,10 @@ import com.uncreative.game.main;
 public class DesktopLauncher {
 	public static void main (String[] arg) {
 		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		config.fullscreen = false;
+		config.title = "Placeholder";
+		config.width = 1920;
+		config.height = 1080;
 		new LwjglApplication(new main(), config);
 	}
 }
