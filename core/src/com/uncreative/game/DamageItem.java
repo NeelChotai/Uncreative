d839bc5aad827f27ec4c20915f11ae76a9ff8ede
KainTenebris <43878231+KainTenebris@users.noreply.github.com>
2018-11-16 11:56:04 +0000

    Create DamageItem
    Fetches implemented
    use not implemented

---
+++ core/src/com/uncreative/game/DamageItem.java
@@ -0,0 +1,36 @@
+
+public class DamageItem implements Item{
+	private Integer damageAmount;
+	private Integer hitChance;
+	private Integer goldWorth;
+	private Integer xpRequired;
+	private Integer uses;
+    
+	public DamageItem(Integer damage, Integer chance, Integer gold, Integer xp, Integer uses) {    
+		this.damageAmount = damage;
+		this.hitChance = chance;
+		this.goldWorth = gold;
+		this.xpRequired = xp;
+		this.uses = uses;
+	}
+	
+	public Integer getDamage() {
+		return this.damageAmount;
+	}
+	public Integer getHitChance() {
+		return this.hitChance;
+	}
+	public Integer getGold() {
+		return this.goldWorth;
+	}
+	public Integer getXP() {
+		return this.xpRequired;
+	}
+	public Integer usesLeft() {
+		return this.uses;
+	}
+	
+	public Boolean use() {
+		return false;
+	}
+}
