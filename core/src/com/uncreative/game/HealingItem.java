fb4fbe6f5749a5b5fc4d23a1deb38246ac989a06
KainTenebris <43878231+KainTenebris@users.noreply.github.com>
2018-11-16 11:35:57 +0000

    Create HealingItem.java
    implemented fetches
    use not implemented

---
+++ core/src/com/uncreative/game/HealingItem.java
@@ -0,0 +1,30 @@
+public class HealingItem implements Item{
+  private Integer goldWorth;
+  private Integer xpRequired;
+  private Integer uses;
+  private Integer healAmount;
+  
+  public void HealingItem(Integer heal, Integer worth, Integer xp, Integer uses){
+    this.healAmount = heal;
+    this.goldWorth = worth;
+    this.xpRequired = xp;
+    this.uses = uses;
+  }
+  
+  public Integer getHealAmount(){
+    return this.healAmount;
+  }
+  public Integer getGold(){
+    return this.goldWorth;
+  }
+  public Integer getXP(){
+    return this.xpRequired;
+  }
+  public Integer usesLeft(){
+    return this.uses;
+  }
+  
+  public Boolean use(){
+    return false;
+  }
+}
