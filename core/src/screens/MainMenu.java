f91106475dbe7dacc1f82608cf46457a7392d1f6
CameronB99 <43375791+CameronB99@users.noreply.github.com>
2018-11-25 15:03:47 +0000

    Main Menu now has a button that plays a sound

--- core/src/screens/MainMenu.java
+++ core/src/screens/MainMenu.java
@@ -2,51 +2,68 @@ package screens;
 
 import com.badlogic.gdx.Game;
 import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.audio.Sound;
 import com.badlogic.gdx.graphics.GL20;
 import com.badlogic.gdx.graphics.Texture;
-import com.badlogic.gdx.graphics.g2d.SpriteBatch;
-import com.badlogic.gdx.math.Matrix4;
+import com.badlogic.gdx.graphics.g2d.TextureRegion;
+import com.badlogic.gdx.scenes.scene2d.InputEvent;
+import com.badlogic.gdx.scenes.scene2d.Stage;
+import com.badlogic.gdx.scenes.scene2d.ui.Button;
+import com.badlogic.gdx.scenes.scene2d.ui.Image;
+import com.badlogic.gdx.scenes.scene2d.ui.TextButton;
+import com.badlogic.gdx.scenes.scene2d.utils.ClickListener;
+import com.badlogic.gdx.scenes.scene2d.utils.TextureRegionDrawable;
+import com.badlogic.gdx.utils.Align;
 
 public class MainMenu extends PirateScreen {
-    private Texture title;
-    private Texture background;
-    private Texture settings;
-    private Texture musicOnOff;
-    private Texture newGame;
-    private Texture loadGame;
-    private Texture quickStart;
-    private SpriteBatch batch;
-
-    private Matrix4 viewMatrix = new Matrix4();
-    private Matrix4 transformMatrix = new Matrix4();
+    private Image title;
+    private Image background;
+    private TextureRegionDrawable newGame;
+    private Stage stage;
+    private Button b_newGame;
+    private Button.ButtonStyle b_newGameStyle;
 
 
     public MainMenu(Game game) {
         super(game);
-        batch = new SpriteBatch();
-        title = new Texture(Gdx.files.internal("drawing.jpg"));
-        background = new Texture(Gdx.files.internal("background.jpg"));
-        newGame = new Texture(Gdx.files.internal("play.png"));
+
+        final Sound ping = Gdx.audio.newSound(Gdx.files.internal("Ping.mp3"));
+
+        stage = new Stage();
+
+        title = new Image(new Texture(Gdx.files.internal("drawing.jpg")));
+        title.setPosition(1920/2, 900, Align.center);
+
+        background = new Image(new Texture(Gdx.files.internal("background.jpg")));
+
+        newGame = new TextureRegionDrawable(new TextureRegion(new Texture(Gdx.files.internal("play.png"))));
+        b_newGameStyle = new TextButton.TextButtonStyle();
+        b_newGameStyle.up = newGame;
+        b_newGameStyle.down = b_newGameStyle.up;
+        b_newGame = new Button(b_newGameStyle);
+        b_newGame.setPosition(1920/2, 1080/2, Align.center);
+        b_newGame.addListener(new ClickListener() {
+           @Override
+           public void clicked(InputEvent event, float x, float y) {
+               System.out.println("BUTTON PRESS :D");
+               ping.play();
+           }
+        });
+
+        stage.addActor(background);
+        stage.addActor(title);
+        stage.addActor(b_newGame);
+        Gdx.input.setInputProcessor(stage);
     }
 
     @Override
     public void render(float delta) {
         Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
-        viewMatrix.setToOrtho2D(0,0,480,320);
-        batch.setProjectionMatrix(viewMatrix);
-        batch.setTransformMatrix(transformMatrix);
-        batch.begin();
-        batch.disableBlending();
-        batch.draw(background, 0, 0, 480, 320, 0, 0, 1920, 1080, false, false);
-        batch.enableBlending();
-        batch.draw(title, 100, 320-128, 480-200, 128, 0, 0, 941, 171, false, false);
-        batch.draw(newGame, 160, 320 - 256, 160, 40, 0, 0, 343, 147, false, false);
-        batch.setBlendFunction(GL20.GL_ONE, GL20.GL_ONE_MINUS_SRC_ALPHA);
-        batch.end();
+        stage.draw();
     }
 
     @Override
-    public void hide() {
-
+    public void dispose() {
+        stage.dispose();
     }
 }
--- desktop/src/com/uncreative/game/desktop/DesktopLauncher.java
+++ desktop/src/com/uncreative/game/desktop/DesktopLauncher.java
@@ -1,5 +1,6 @@
 package com.uncreative.game.desktop;
 
+import com.badlogic.gdx.Graphics.DisplayMode;
 import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
 import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
 import com.uncreative.game.Pirates;
@@ -7,10 +8,9 @@ import com.uncreative.game.Pirates;
 public class DesktopLauncher {
 	public static void main (String[] arg) {
 		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
-		config.fullscreen = true;
-		config.title = "Placeholder";
-		config.width = 1920;
-		config.height = 1080;
+		DisplayMode mode = LwjglApplicationConfiguration.getDesktopDisplayMode();
+		config.setFromDisplayMode(mode);
+		config.title = "Pirates!";
 		new LwjglApplication(new Pirates(), config);
 	}
 }
