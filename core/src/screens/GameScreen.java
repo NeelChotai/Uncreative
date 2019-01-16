329779f3d245692f44966df785cd2f5a03568e65
Cameron Bell <crb551@york.ac.uk>
2019-01-16 17:36:16 +0000

    Removed the redundant main class and added code for the game camera/map

--- core/src/screens/GameScreen.java
+++ core/src/screens/GameScreen.java
@@ -1,17 +1,67 @@
 package screens;
 
 import com.badlogic.gdx.Game;
-import com.uncreative.game.Location;
-import com.uncreative.game.Pirates;
+import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.graphics.GL20;
+import com.badlogic.gdx.graphics.OrthographicCamera;
+import com.badlogic.gdx.maps.MapProperties;
+import com.badlogic.gdx.maps.tiled.TiledMap;
+import com.badlogic.gdx.maps.tiled.TiledMapRenderer;
+import com.badlogic.gdx.maps.tiled.TmxMapLoader;
+import com.badlogic.gdx.maps.tiled.renderers.OrthogonalTiledMapRenderer;
+import com.uncreative.game.*;
+
+import java.util.ArrayList;
 
 public class GameScreen extends PirateScreen{
+    TiledMap tiledMap;
+    TiledMapRenderer mapRenderer;
+    OrthographicCamera camera;
+    MapProperties properties;
+    PlayerShip player;
 
     public GameScreen(Game game) {
         super(game);
-        for(int i = 0; i < Pirates.size; i++) {
-            for(int j = 0; j < Pirates.size; j++) {
-                Pirates.map[i][j] = new Location(i, j);
+
+        //Create instances of the map and camera.
+        tiledMap = new TmxMapLoader().load("map.tmx");
+        mapRenderer = new OrthogonalTiledMapRenderer(tiledMap);
+        camera = new OrthographicCamera();
+        camera.setToOrtho(false, Pirates.w, Pirates.h);
+        camera.update();
+
+        //Create an array of Location objects for each tile.
+        properties = tiledMap.getProperties();
+        Pirates.map = new Location[properties.get("width", Integer.class)][properties.get("height", Integer.class)];
+        for(int x = 0; x < properties.get("width", Integer.class); x++) {
+            for(int y = 0; y < properties.get("height", Integer.class); y++) {
+                Pirates.map[x][y] = new Location(x,y);
             }
         }
+
+        Buff Goodrickebuff1 = new Buff("attack", 1, 1, true);
+        ArrayList<Buff>  goodrickeBuffs = new ArrayList<Buff>();
+        goodrickeBuffs.add(Goodrickebuff1);
+        College goodricke = new College("Goodricke",100, 100, true, goodrickeBuffs);
+        player = new PlayerShip(10, 10, 5, 2, goodricke, 1000, 0, new Item[0], Pirates.map[2][2]);
+    }
+
+    @Override
+    public void render(float delta) {
+        Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
+        setCameraToPlayer();
+        camera.update();
+        mapRenderer.setView(camera);
+        mapRenderer.render();
+    }
+
+    @Override
+    public void dispose() {
+        tiledMap.dispose();
+    }
+
+    public void setCameraToPlayer() {
+        camera.position.x = player.location.getLocation()[0]*properties.get("tilewidth", Integer.class);
+        camera.position.y = player.location.getLocation()[1]*properties.get("tileheight", Integer.class);
     }
 }
