c4b763d589c9b96a99c94463c0b23bb51e9d6230
Cameron Bell <crb551@york.ac.uk>
2019-01-19 22:48:44 +0000

    Death Screen now returns you to the main menu after pressing a button

--- core/src/screens/deathScreen.java
+++ core/src/screens/deathScreen.java
@@ -2,6 +2,7 @@ package screens;
 
 import com.badlogic.gdx.Game;
 import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.InputProcessor;
 import com.badlogic.gdx.graphics.Color;
 import com.badlogic.gdx.graphics.GL20;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
@@ -17,7 +18,48 @@ public class deathScreen extends PirateScreen {
         this.score = score;
         time = 0;
         batch = new SpriteBatch();
+        Gdx.input.setInputProcessor(new InputProcessor() {
+            @Override
+            public boolean keyDown(int keycode) {
+                return false;
+            }
 
+            @Override
+            public boolean keyUp(int keycode) {
+                toMainMenu();
+                return false;
+            }
+
+            @Override
+            public boolean keyTyped(char character) {
+                return false;
+            }
+
+            @Override
+            public boolean touchDown(int screenX, int screenY, int pointer, int button) {
+                return false;
+            }
+
+            @Override
+            public boolean touchUp(int screenX, int screenY, int pointer, int button) {
+                return false;
+            }
+
+            @Override
+            public boolean touchDragged(int screenX, int screenY, int pointer) {
+                return false;
+            }
+
+            @Override
+            public boolean mouseMoved(int screenX, int screenY) {
+                return false;
+            }
+
+            @Override
+            public boolean scrolled(int amount) {
+                return false;
+            }
+        });
     }
 
     public void render(float delta) {
@@ -27,5 +69,9 @@ public class deathScreen extends PirateScreen {
         super.font.setColor(Color.WHITE);
         super.font.draw(batch, "GAME OVER\nSCORE: " + score, Pirates.w/2f, Pirates.h/2f);
         batch.end();
+    }
+
+    public void toMainMenu() {
+        game.setScreen(new MainMenu(game));
     }
 }
